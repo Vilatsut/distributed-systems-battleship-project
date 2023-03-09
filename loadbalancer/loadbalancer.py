9ab9edf3936706070b5dcf6cb5bfdf47dfdd477c
Lauri Suutari <lsuutari19@student.oulu.fi>
2023-03-09 19:37:37 +0200

    redis initial implementation for loadbalancer

--- loadbalancer/loadbalancer.py
+++ loadbalancer/loadbalancer.py
@@ -1,5 +1,5 @@
 import socket
-
+import redis
 
 HEADER = 4096
 PORT = 16432
@@ -23,16 +23,23 @@ chat_online = False
 while True:
     client_socket, client_address = load_balancer.accept()
     data = client_socket.recv(1024)
-    if "Game server" in data.decode():
-        print(f'Received from {client_address}: {data.decode()}')
-        game_server_addresses.append(data.decode()[-4:])
-    if "Chat server" in data.decode():
-        print(f"Chat server is online.")
-        chat_online = True
+    if "reconnect" in data.decode():
+        # if the last 4 string letters are the gamecode
+        # there needs to be a number in Redis() if each gameserver has a different db 
+        redis = redis.Redis()
+        redis.mget(data.decode()[-4:])
+        pass
     else:
-        print("Client connected!")
-        response = f"There are currently {len(game_server_addresses)} game servers active in addresses: \n {''.join(str(game_server_addresses))}"
-        if chat_online:
-            response = response + ". and the Chat Server is online."
-        client_socket.send(response.encode())
+        if "Game server" in data.decode():
+            print(f'Received from {client_address}: {data.decode()}')
+            game_server_addresses.append(data.decode()[-4:])
+        if "Chat server" in data.decode():
+            print(f"Chat server is online.")
+            chat_online = True
+        else:
+            print("Client connected!")
+            response = f"There are currently {len(game_server_addresses)} game servers active in addresses: \n {''.join(str(game_server_addresses))}"
+            if chat_online:
+                response = response + ". and the Chat Server is online."
+            client_socket.send(response.encode())
     client_socket.close()
